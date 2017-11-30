7c8f3158bd380682183062382b07f8a61182cd3c
Kornel Dubieniecki <lekterable@gmail.com>
2017-11-30 20:43:29 +0100

    Przygotowanie do Heroku

--- app.js
+++ app.js
@@ -104,6 +104,6 @@ app.post('/', (req, res)=>{
   }).end()
 })
 
-app.listen(3000, ()=>{
+app.listen(process.env.PORT || 3000, ()=>{
   console.log('Serwer uruchomiony na porcie 3000')
 })
--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-  database: 'mongodb://localhost:27017/weatherapp',
+  database: process.env.MONGODB_URI || 'mongodb://localhost:27017/weatherapp',
   secret: 'yoursecret'
 }
